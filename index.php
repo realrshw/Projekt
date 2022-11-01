6257d41d3dc1aa7ba76be41664b2045653a85af7
cdrCraft <cdrcraft.mc@gmail.com>
2022-11-01 11:00:57 +0100

    Startseite verbessert

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instagrum</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Instagrum</title>
+    </head>
+    <body>
 
-<?php
-echo "<h1>Überschrift</h1>";
-echo "Hi";
-echo "<p>schöner text.</p>";
-?>
-    
-</body>
+        <?php
+        #Story
+        #Bilder
+        #footer
+        ?>
+            
+    </body>
 </html>
