eb1dd748af3f1a08d686eacb07118b673b0748d1
JonathanUloth <116708040+JonathanUloth@users.noreply.github.com>
2022-11-01 11:42:30 +0100

    Team jonathan startseite (#10)
    * schönen Text hinzugefügt
    
    * Startseite verbessert
    
    * test
    
    * index aktualisiert
    
    Co-authored-by: cdrCraft <cdrcraft.mc@gmail.com>

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instagrum</title>
-</head>
-<body>
-
-<?php
-echo "<h1>Überschrift</h1>";
-echo "Hi";
-?>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Instagrum</title>
+    </head>
     
-</body>
+    <body>
+
+        <?php
+
+        #wenn nicht login --> weiter zu login seite
+
+        #Story
+        #Bilder
+        #footer
+
+        ?>
+            
+    </body>
 </html>
