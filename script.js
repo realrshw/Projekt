cf9e026d5c70ab7bccbe55256e353e7ccc5eb9de
Mika <79707824+zLyxn@users.noreply.github.com>
2022-11-18 12:11:57 +0100

    login (#32)
    fertig
    
    Signed-off-by: Bommberk <83213983+Bommberk@users.noreply.github.com>
    
    Signed-off-by: Bommberk <83213983+Bommberk@users.noreply.github.com>
    Signed-off-by: Merlin0006 <116671992+Merlin0006@users.noreply.github.com>
    Co-authored-by: Bommberk <83213983+Bommberk@users.noreply.github.com>
    Co-authored-by: Merlin0006 <116671992+Merlin0006@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,6 @@
+function register(){
+    document.getElementById("weg1").classList.toggle("weg");
+    document.getElementById("weg2").classList.toggle("weg2");
+    document.getElementById("hin1").classList.toggle("hin");
+    document.getElementById("hin2").classList.toggle("hin");
+}
