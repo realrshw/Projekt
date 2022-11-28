8229feeb7b473fa7f61eb41e8b2f1074e922ee0a
Merlin0006 <116671992+Merlin0006@users.noreply.github.com>
2022-11-28 09:08:51 +0000

    Pfad hoffentlich angepasst

--- functions.php
+++ functions.php
@@ -46,7 +46,11 @@
 
         while($i < $j){
             if(!file_exists("assets/userdata/$i")){
-                mkdir("assets/userdata/$i","0700", true);break;
+                mkdir("/assets/userdata/$i","0700", true);break;
+                mkdir("/assets/userdata/$i/benutzerdaten", "0700",true);
+                mkdir("/assets/userdata/$i/post", "0700",true);
+                mkdir("/assets/userdata/$i/abonnierte", "0700",true);
+                mkdir("/assets/userdata/$i/messenger", "0700",true);
             }else{
                 $i++;
             }
@@ -66,27 +70,27 @@
         #Cookie anlegen
         setcookie('userid', '$zahl', time()+3600000);
 
-        $datei_pass = fopen("assets/userdata/$zahl/passwort.txt","w+");
+        $datei_pass = fopen("/assets/userdata/$zahl/benutzerdaten/passwort.txt","w+");
         fwrite($datei_pass,$passwort);
         fclose($datei_pass);
         
-        $datei_email = fopen("assets/userdata/$zahl/email.txt","w+");
+        $datei_email = fopen("/assets/userdata/$zahl/benutzerdaten/email.txt","w+");
         fwrite($datei_email,$email);
         fclose($datei_email);
 
-        $datei_vorname = fopen("assets/userdata/$zahl/vorname.txt","w+");
+        $datei_vorname = fopen("/assets/userdata/$zahl/benutzerdaten/vorname.txt","w+");
         fwrite($datei_vorname,$vorname);
         fclose($datei_vorname);
         
-        $datei_nachname = fopen("assets/userdata/$zahl/nachname.txt","w+");
+        $datei_nachname = fopen("/assets/userdata/$zahl/benutzerdaten/nachname.txt","w+");
         fwrite($datei_nachname,$nachname);
         fclose($datei_nachname);
 
-        $datei_user = fopen("assets/userdata/$zahl/user.txt","w+");
+        $datei_user = fopen("/assets/userdata/$zahl/benutzerdaten/user.txt","w+");
         fwrite($datei_user,$user);
         fclose($datei_user);
 
-        $datei_user_id = fopen("assets/userdata/$zahl/user_id.txt","w+");
+        $datei_user_id = fopen("/assets/userdata/$zahl/benutzerdaten/user_id.txt","w+");
         fwrite($datei_user_id,$user_id);
         fclose($datei_user_id);
 
