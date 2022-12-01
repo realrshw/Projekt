5b1a7764f5a445ba42f13730e5af2a930bd198c3
Wapple <116707883+69devin@users.noreply.github.com>
2022-12-01 08:19:25 +0000

    Pfad bilder angepasst

--- unterseiten/start.php
+++ unterseiten/start.php
@@ -9,7 +9,7 @@
 <div class="headline">
 
 <div class="logo">
-      <a href="projekt.php"><img src="instagramlogo.jpg" width="130" height="50"></a>
+      <a href="projekt.php"><img src="../assets/img/start/instagramlogo.jpg" width="130" height="50"></a>
 </div>
       <form>
             <input type="Text" name="" value="" size="40" placeholder="Suchen">
@@ -20,42 +20,42 @@
       </div>
 
       <div class="teilen">
-            <a href=""><img src="teilen.jpg" width="25" height="25"</a>
+            <a href=""><img src="../assets/img/start/teilen.jpg" width="25" height="25"</a>
       </div>
 
       <div class="kompass">
-            <a href=""><img src="kompass.jpg" width="25" height="25"></a>
+            <a href=""><img src="../assets/img/start/kompass.jpg" width="25" height="25"></a>
       </div>
 
       <div class="herz">
-            <a href=""><img src="herz.jpg" width="30" height="30"></a>
+            <a href=""><img src="../assets/img/start/herz.jpg" width="30" height="30"></a>
       </div>
         <div class="beitrag">
 
             Nassenstein.Insta
 
       <div class="beitrag1">
-            <img src="beitrag1.png" width="900" height="900" border="0" alt="">
+            <img src="../assets/img/start/beitrag1.png" width="900" height="900" border="0" alt="">
       </div>
 
       <div class="herzbeitrag">
-            <img id="heart" src="herz.jpg" width="40" height="40" onClick="heart()">
+            <img id="heart" src="../assets/img/start/herz.jpg" width="40" height="40" onClick="heart()">
       </div>
 
       <div class="dreipunkte">
-            <a href=""><img src="dreipunkte.jpg" width="30" height="30"></a>
+            <a href=""><img src="../assets/img/start/dreipunkte.jpg" width="30" height="30"></a>
       </div>
 
       <div class="komentar">
-            <a href=""><img src="komentar.png" width="25" height="25"></a>
+            <a href=""><img src="../assets/img/start/komentar.png" width="25" height="25"></a>
       </div>
 
       <div class="teilenbeitrag">
-            <a href=""><img src="teilen.jpg" width="30" height="30"></a>
+            <a href=""><img src="../assets/img/start/teilen.jpg" width="30" height="30"></a>
       </div>
 
       <div class="lesezeichen">
-            <a href=""><img src="lesezeichen-removebg-preview.png" width="35" height="27"></a>
+            <a href=""><img src="../assets/img/start/lesezeichen-removebg-preview.png" width="35" height="27"></a>
       </div>
 
 
