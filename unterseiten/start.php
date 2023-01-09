1d76366175b808c9d90467eaf3404df6d7795155
JonathanUloth <116708040+JonathanUloth@users.noreply.github.com>
2023-01-09 10:11:42 +0100

    Die fertige Startseite. (#57)
    Signed-off-by: JonathanUloth <116708040+JonathanUloth@users.noreply.github.com>
    
    Signed-off-by: JonathanUloth <116708040+JonathanUloth@users.noreply.github.com>

--- unterseiten/start.php
+++ unterseiten/start.php
@@ -1,80 +1,111 @@
-<html>
-    <head>
-        <title>Instagram</title>
-        <link rel="stylesheet" href="../assets/css/start.css">
-    </head>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-           <body bgcolor="#EFEFEF">
+      <title>Document</title>
+
+      <link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="assets/css/home.css">
+</head>
+
+<body>
 
 <div class="headline">
 
-<div class="logo">
-      <a href="projekt.php"><img src="../assets/img/start/instagramlogo.jpg" width="130" height="50"></a>
-</div>
+      <div class="logo">
+            <a href="projekt.php"><img src="assets/img/icon/instagramlogo.jpg" width="130" height="50"></a>
+      </div>
       <form>
             <input type="Text" name="" value="" size="40" placeholder="Suchen">
       </form>
 
       <div class="haus">
-            <a href="projekt.php"><img src="haus.jpg" width="25" height="25""></a>
+            <a href="projekt.php"><img src="assets/img/icon/haus.jpg" width="25" height="25"></a>
       </div>
 
       <div class="teilen">
-            <a href=""><img src="../assets/img/start/teilen.jpg" width="25" height="25"</a>
+            <a href=""><img src="assets/img/icon/teilen.jpg" width="25" height="25"></a>
       </div>
 
       <div class="kompass">
-            <a href=""><img src="../assets/img/start/kompass.jpg" width="25" height="25"></a>
+            <a href=""><img src="assets/img/icon/kompass.jpg" width="25" height="25"></a>
       </div>
 
       <div class="herz">
-            <a href=""><img src="../assets/img/start/herz.jpg" width="30" height="30"></a>
+            <a href=""><img src="assets/img/icon/herz.jpg" width="30" height="30"></a>
       </div>
-        <div class="beitrag">
+        
 
-            Nassenstein.Insta
+            <?php
+            
+            include("functions.php");
+            echo "<script language=\"javascript\" type=\"text/javascript\" src=\"assets/js/home.js\"></script>";
+            for($i=1;$i<100;$i++){
 
-      <div class="beitrag1">
-            <img src="../assets/img/start/beitrag1.png" width="900" height="900" border="0" alt="">
-      </div>
+            $ausgabe = ausgeben();
 
-      <div class="herzbeitrag">
-            <img id="heart" src="../assets/img/start/herz.jpg" width="40" height="40" onClick="heart()">
-      </div>
 
+            $post_text = $ausgabe["a"];
+            $userID = $ausgabe["b"];
+            $postID = $ausgabe["c"];
+            $Like_anzahl = $ausgabe["d"];
+
+            echo "
+            <div class=\"beitrag\">
+                  <div class=\"Text_des_Beitrags\">
+                        $post_text
+                  </div>
+                  
+                  <div id=\"\">
+                        <div class=\"Like_anzahl\">
+                              <span>$Like_anzahl</span>
+                        </div>
+
+                        <div id=\"Herz\">
+                              <a href=\"like.php?userID=$userID&postID=$postID\">
+                                    <img class=\"heart\" id=\"heart$i\" src=\"assets/img/icon/herz.png\" width=\"40\" height=\"40\" onClick=\"heart($i)\">
+                              </a>
+                        </div>
+
+                        <div class=\"comment\">
+                              <img src=\"assets/img/icon/komentar.png\" class=\"comment\" onClick=\"comment($i)\">
+                        </div>
+                        
+                        
+                  </div>
+            </div>
+            ";
+            }
+            #<a href="comment-save.php?user=$user&comment=$comment&creator=$creator&post=$post"></a>
+            ?>
+            
+
+
+      
+<!--
       <div class="dreipunkte">
-            <a href=""><img src="../assets/img/start/dreipunkte.jpg" width="30" height="30"></a>
+            <a href=""><img src="assets/img/icon/dreipunkte.jpg" width="30" height="30"></a>
       </div>
 
-      <div class="komentar">
-            <a href=""><img src="../assets/img/start/komentar.png" width="25" height="25"></a>
-      </div>
+
 
       <div class="teilenbeitrag">
-            <a href=""><img src="../assets/img/start/teilen.jpg" width="30" height="30"></a>
+            <a href=""><img src="assets/img/icon/teilen.jpg" width="30" height="30"></a>
       </div>
 
       <div class="lesezeichen">
-            <a href=""><img src="../assets/img/start/lesezeichen-removebg-preview.png" width="35" height="27"></a>
+            <a href=""><img src="assets/img/icon/lesezeichen-removebg-preview.png" width="35" height="27"></a>
       </div>
 
 
-
-
+      -->
       </div>
 
       </div>
 
-       <script>
-           var herz = document.getElementById("heart");
 
-        function heart(){
-            document.getElementById("heart").src="herzrot.jpg";
-            document.getElementById("heart").style.width="40px";
-            document.getElementById("heart").style.height="40px";
-        }
-
-    </script>
 </body>
 </html>
-
